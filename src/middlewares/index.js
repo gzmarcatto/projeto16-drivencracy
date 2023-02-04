6f42b887217a8f485fbd1ac000d052b9bc6f5848
Giovanni Marcatto <gzmarcatto@gmail.com>
2023-02-04 09:54:05 -0300

    fix: error message on index.js

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,10 +1,10 @@
 export function validateSchemaMiddleware(schema) {
   return (req, res, next) => {
     const { error } = schema.validate(req.body);
+    next();
     if (error) {
       const error = error.details[0].message;
       res.status(422).send(error);
     }
-    next();
   };
 }
